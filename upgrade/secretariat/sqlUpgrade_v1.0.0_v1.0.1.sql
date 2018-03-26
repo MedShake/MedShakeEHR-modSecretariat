27a57ad84fceb0676e004846bc9ac29b594bed90
fr33z00 <frisou76@yahoo.fr>
2018-03-26 20:25:32 +0100

    Améliorations esthétiques

---
+++ upgrade/secretariat/sqlUpgrade_v1.0.0_v1.0.1.sql
@@ -0,0 +1,4 @@
+-- Modifications des données de la bdd de v1.0.0. à v1.0.1
+
+UPDATE `system` SET `value`='v1.0.1' WHERE `groupe`='module' and `name`='secretariat';
+
---
+++ upgrade/secretariat/sqlUpgrade_v1.0.1_v1.1.0.sql
@@ -0,0 +1,4 @@
+-- Modifications des données de la bdd de v1.0.1. à v1.1.0
+
+UPDATE `system` SET `value`='v1.1.0' WHERE `groupe`='module' and `name`='secretariat';
+
