84c59f3b2ea29652d3c78fddfc76555fc811da12
fr33z00 <frisou76@yahoo.fr>
2018-04-02 22:19:54 +0100

    Corrections du fichier de mise à jour

--- upgrade/secretariat/sqlUpgrade_v1.0.1_v1.1.0_post.php
+++ upgrade/secretariat/sqlUpgrade_v1.0.1_v1.1.0_post.php
@@ -1,7 +1,7 @@
 <?php
 $file_list=[
-    $p['homepath'].'controlers/module/secretatiat/configuration/actions/inc-ajax-configDefaultUsersParams.php',
-    $p['homepath'].'controlers/module/secretatiat/configuration/configDefaultParams.php'
+    $p['homepath'].'controlers/module/secretariat/configuration/actions/inc-ajax-configDefaultUsersParams.php',
+    $p['homepath'].'controlers/module/secretariat/configuration/configDefaultParams.php'
 ];
 foreach ($file_list as $file) {
   if (is_file($file))
