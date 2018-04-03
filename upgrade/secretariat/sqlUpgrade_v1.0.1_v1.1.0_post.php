246285bf46d3849576f48b526ca39cf47dea7349
fr33z00 <frisou76@yahoo.fr>
2018-04-03 07:21:23 +0100

    Suppression de fichiers devenus inutiles

--- upgrade/secretariat/sqlUpgrade_v1.0.1_v1.1.0_post.php
+++ upgrade/secretariat/sqlUpgrade_v1.0.1_v1.1.0_post.php
@@ -3,8 +3,15 @@ $file_list=[
     $p['homepath'].'controlers/module/secretariat/configuration/actions/inc-ajax-configDefaultUsersParams.php',
     $p['homepath'].'controlers/module/secretariat/configuration/configDefaultParams.php'
 ];
+$dir_list=[
+    $p['config']['templatesFolder'].'secretariat/configuration'
+];
 foreach ($file_list as $file) {
   if (is_file($file))
     unlink($file);
+}
+foreach ($dir_list as $dir) {
+  if (is_dir($dir))
+    rmdir($dir);
 }
 
